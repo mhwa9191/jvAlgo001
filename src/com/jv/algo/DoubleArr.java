cbbae170db0cade7a5359a6b32250220e48f9010
mhwa9191 <mmhhwa@gmail.com>
2023-05-04 21:45:15 +0900

    double arr length

--- src/com/jv/algo/DoubleArr.java
+++ src/com/jv/algo/DoubleArr.java
@@ -8,5 +8,15 @@ public class DoubleArr {
 		
 		//System.out.println(arr1.length); //3
 		//System.out.println(arr1[0].length); //2
+
+		// arr1*arr2 °á°ú
+		int[][] arr=new int[arr1.length][arr2[0].length];
+		
+		
 	}
+	
 }
+
+// 00 01  00 01 = 00*00+01*10 00*10+01*11
+// 10 11  10 11   10*00+11*10 10*10+11*11
+// 20 21          20*00+21*10 20*10+21*11
