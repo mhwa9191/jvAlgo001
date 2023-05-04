1dcb328d8d879ccbcc4defae72da437ffbdff864
mhwa9191 <mmhhwa@gmail.com>
2023-05-04 21:40:54 +0900

    double arr

---
+++ src/com/jv/algo/DoubleArr.java
@@ -0,0 +1,12 @@
+package com.jv.algo;
+
+public class DoubleArr {
+
+	public static void main(String[] args) {
+		int[][] arr1= {{1,4},{2,3},{4,1}}; 
+		int[][] arr2= {{2,3},{1,4}};
+		
+		//System.out.println(arr1.length); //3
+		//System.out.println(arr1[0].length); //2
+	}
+}
