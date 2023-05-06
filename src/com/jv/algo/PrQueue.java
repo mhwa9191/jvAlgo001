de254b223e94e5e15cafcb13c0c0ff9b8f804793
mhwa9191 <mmhhwa@gmail.com>
2023-05-06 11:07:27 +0900

    priority quque test

--- src/com/jv/algo/PrQueue.java
+++ src/com/jv/algo/PrQueue.java
@@ -30,5 +30,31 @@ public class PrQueue {
 		// 1
 		
 		System.out.println("============="); 
+
+		int[] list= {10,20,40};
+		testPrQ(list);
 	}
+
+	private static void testPrQ(int[] list) {
+		int output=0;
+		PriorityQueue<Integer> testQ = new PriorityQueue<Integer>();
+		
+		//우선순위 큐로 바꿈
+		for (Integer in : list) {
+			testQ.add(in);
+		}
+		
+		//카드의 개수가 1이 아닐때 까지 반복
+		while (testQ.size() != 1) {
+			int first = testQ.poll(); //우선순위로 뽑기
+			int second = testQ.poll();
+			int sum= first+second;
+			output+=sum;
+			testQ.add(sum);
+//			System.out.println(testQ);
+		}
+		System.out.println(output);
+	}
+	
+	
 }
