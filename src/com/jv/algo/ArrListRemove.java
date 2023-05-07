411769bad95473b2cbadb772430b3afdbe64e43d
mhwa9191 <mmhhwa@gmail.com>
2023-05-07 19:01:23 +0900

    arrlist remove Map get 사용시 위치 값 차이

--- src/com/jv/algo/ArrListRemove.java
+++ src/com/jv/algo/ArrListRemove.java
@@ -49,7 +49,27 @@ public class ArrListRemove {
 		int indexDel3=arrlist3.indexOf(del3);
 		arrlist3.remove(indexDel3);
 		System.out.println("arrlist3 "+arrlist3);
-		System.out.println("=============");
+		System.out.println("=============Map get 사용시 위치 값 차이");
 		
+		Map<Integer, Integer> mapp=new HashMap<Integer, Integer>();
+		ArrayList<Integer> mappArr = new ArrayList<Integer>(List.of(4,3,1,2));
+		mapp.put(4, 0);
+		mapp.put(3, 1);
+		mapp.put(1, 2);
+		mapp.put(2, 3);
+		System.out.println(mapp);
+		mappArr.remove(mapp.get(3));
+		System.out.println("mappArr "+mappArr); //mappArr [4, 3, 2]
+		
+		Map<Integer, Integer> mapp2=new HashMap<Integer, Integer>();
+		ArrayList<Integer> mappArr2 = new ArrayList<Integer>(List.of(4,3,1,2));
+		mapp2.put(4, 0);
+		mapp2.put(3, 1);
+		mapp2.put(1, 2);
+		mapp2.put(2, 3);
+		System.out.println(mapp2);
+		int mapIndex=mapp2.get(3);
+		mappArr2.remove(mapIndex);
+		System.out.println("mappArr2 "+mappArr2); //mappArr2 [4, 1, 2]
 	}
 }
