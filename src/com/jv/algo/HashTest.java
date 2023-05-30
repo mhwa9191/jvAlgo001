52ea90dd0880d89d524fe1113a07d48d29069ed4
mhwa9191 <mmhhwa@gmail.com>
2023-05-30 11:25:49 +0900

    해시 getOrDefault

--- src/com/jv/algo/HashTest.java
+++ src/com/jv/algo/HashTest.java
@@ -12,26 +12,29 @@ public class HashTest {
 		Map<String, Integer> map=new HashMap<String, Integer>();
 		
 		for(String p:ps) {
-			if(map.containsKey(p)) {
-				int add= map.get(p)+1;
-				map.put(p, add);
-			}else {
-				map.put(p, 1);
-			}
+//			if(map.containsKey(p)) {
+//				int add= map.get(p)+1;
+//				map.put(p, add);
+//			}else {
+//				map.put(p, 1);
+//			}
+			map.put(p, map.getOrDefault(p, 0)+1);
 		}
 		System.out.println("map "+map);
 
 		for(String c:cs) {
-			if(map.containsKey(c)) {
-				int minus= map.get(c)-1;
-				map.put(c, minus);
-			}
+//			if(map.containsKey(c)) {
+//				int minus= map.get(c)-1;
+//				map.put(c, minus);
+//			}
+			map.put(c, map.get(c)-1);
 		}
 		System.out.println("map "+map);
 		
 		for(String k: map.keySet()) {
 			if(map.get(k)>0) {
 				System.out.println(k);
+				break;
 			}
 		}
 	}
