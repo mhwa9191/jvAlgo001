bba0ddde04529de8ebaff6d2ceaa217c13356cb7
mhwa9191 <mmhhwa@gmail.com>
2023-07-12 13:35:41 +0900

    report 정확도 45 set iterator

--- src/com/jv/algosol/ReportTest.java
+++ src/com/jv/algosol/ReportTest.java
@@ -1,7 +1,11 @@
 package com.jv.algosol;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ReportTest {
 	public static void main(String[] args) {
@@ -16,8 +20,12 @@ public class ReportTest {
 		System.out.println("sol1  :: "+Arrays.toString(solution(id_list1,report1,k1))); //[2,1,1,0]
 		System.out.println("sol2  :: "+Arrays.toString(solution(id_list2,report2,k2))); //[0,0]
 
+		System.out.println();
+		System.out.println("//sol1  :: "+Arrays.toString(solution2(id_list1,report1,k1))); //[2,1,1,0]
+		System.out.println("//sol2  :: "+Arrays.toString(solution2(id_list2,report2,k2))); //[0,0]
 	}
 
+	//정확도 29
 	public static int[] solution(String[] id_list, String[] report, int k) {
 		int[] answer= new int[id_list.length];
 		int[] result= new int[id_list.length];
@@ -46,12 +54,14 @@ public class ReportTest {
 			String reported_user=temp[1];
 			if(map2.get(report_user)==null) {
 				map2.put(report_user, reported_user);
+			}else if(map2.get(report_user).contains(reported_user)) {
+				continue;
 			}else {
 				map2.put(report_user, map2.get(report_user)+" "+reported_user);
 			}
 		}
-//		System.out.println(map);
-//		System.out.println("map2 :: "+map2);
+		System.out.println("map key를 신고한 유저들 "+map);
+		System.out.println("map2 :: "+map2);
 		
 		for(int i=0; i<id_list.length; i++) {
 			if(map.get(id_list[i])==null) {
@@ -92,6 +102,66 @@ public class ReportTest {
 			}
 			
 		}		
+		
+		return answer;
+	}
+	
+	
+	//정확도 45
+	public static int[] solution2(String[] id_list, String[] report, int k) { 
+		int[] answer= new int[id_list.length];
+		 
+	    Set<String> set = new HashSet<>();
+	    for (String str : report) {
+	        set.add(str);
+	    }
+	    System.out.println("set "+set);
+
+	    HashMap<String, String> map=new HashMap<String, String>();
+		HashMap<String, String> map2=new HashMap<String, String>(); //내가 신고한 목록
+		//신고된 유저 목록
+		ArrayList<String> list=new ArrayList<String>();
+		Iterator<String> iter=set.iterator();		
+		while(iter.hasNext()) {
+			String[] temp=iter.next().split(" ");
+			String report_user=temp[0];
+			String reported_user=temp[1];
+			list.add(reported_user);
+			if(map2.get(report_user)==null) {
+				map2.put(report_user, reported_user);
+			}else {
+				map2.put(report_user, map2.get(report_user)+" "+reported_user);
+			}
+		}
+		System.out.println("map2 :: "+map2);
+		System.out.println("list   "+list);
+		
+		
+		HashMap<String, Integer> id_report = new HashMap<String, Integer>();
+		for(int i=0; i<id_list.length; i++) {
+			int num=0;
+			for(String str:list) {
+				if(id_list[i].equals(str)) {
+					num++;
+				}
+			}
+			id_report.put(id_list[i], num);
+		}
+		System.out.println("id_report  "+id_report);
+		
+		//결과 알림
+		for(int i=0; i<id_list.length; i++) {
+			if(map2.get(id_list[i])==null) {
+				answer[i]=0;
+			}else {
+				for(String str : map2.get(id_list[i]).split(" ")) {
+					if(id_report.get(str)>=k) {
+						answer[i]++;
+					}
+				}
+			}
+			
+		}
 		
 		return answer;
 	}
