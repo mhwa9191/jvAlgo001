042465cabeab81fc80a8909ee7ad52e4676a6289
mhwa9191 <mmhhwa@gmail.com>
2023-07-18 11:42:43 +0900

    제곱근 범위 오류 수정

--- src/com/jv/algosol/SquareRoot.java
+++ src/com/jv/algosol/SquareRoot.java
@@ -5,9 +5,13 @@ public class SquareRoot {
 	public static void main(String[] args) {
 		long num1 = 121;
 		long num2 = 3;
+//		long num3 = 5000000000; out of range!!
 		
 		System.out.println("sol1 :: "+solution(num1));
 		System.out.println("sol2 :: "+solution(num2));
+
+		System.out.println("2 sol1 :: "+solution2(num1));
+		System.out.println("2 sol2 :: "+solution2(num2));
 		
 	}
 	
@@ -22,6 +26,20 @@ public class SquareRoot {
             }
         }
         
+        return answer;
+	}
+
+	public static long solution2(long n) {
+        long answer = 0;
+        double sqr=Math.sqrt(n);        
+        if((sqr*10)%10==0) {
+//        	System.out.println(sqr+1);
+//        	System.out.println(Math.round(sqr));
+        	answer=(Math.round(sqr)+1)*(Math.round(sqr)+1);
+        }else {
+        	answer=-1;
+        }
+               
         return answer;
 	}
 }
